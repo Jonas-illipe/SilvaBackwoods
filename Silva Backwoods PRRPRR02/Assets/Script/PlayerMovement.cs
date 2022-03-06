6691843749241eff4fa4200d52bc6926eccffae7
Jonas <jonas.illipe@elev.ga.lbs.se>
2022-03-06 22:30:16 +0100

    Wallgrabbing

--- Silva Backwoods PRRPRR02/Assets/Script/PlayerMovement.cs
+++ Silva Backwoods PRRPRR02/Assets/Script/PlayerMovement.cs
@@ -10,19 +10,23 @@ public class PlayerMovement : MonoBehaviour
 
     private Rigidbody2D rb;
 
-    public Transform groundCheckPoint;//, wallGrabPoint;
+    public Transform groundCheckPoint;
     public LayerMask whatIsGround;
     private bool isGrounded;
 
-    public Animator anim;
 
     public Transform wallGrabPoint;
-    private bool canGrab, isGrabing;
+    private bool canGrab, isGrabbing;
+    private float gravityStore;
     //public SpriteRenderer playerSR;
+
+
+    public Animator anim;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        gravityStore = rb.gravityScale;
     }
 
 
@@ -32,7 +36,7 @@ public class PlayerMovement : MonoBehaviour
         rb.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * moveSpeed, rb.velocity.y);
 
         //Check if player is on the ground
-        isGrounded = Physics2D.OverlapCircle(groundCheckPoint.position, checkPointRadius, whatIsGround); //|| Physics2D.OverlapCircle(wallGrabPoint.position, checkPointRadius, whatIsGround);
+        isGrounded = Physics2D.OverlapCircle(groundCheckPoint.position, checkPointRadius, whatIsGround) || Physics2D.OverlapCircle(wallGrabPoint.position, checkPointRadius, whatIsGround);
 
         //Jump upwards
         if (Input.GetButtonDown("Jump") && isGrounded)
@@ -44,28 +48,44 @@ public class PlayerMovement : MonoBehaviour
         if (Input.GetAxisRaw("Horizontal") < 0)
         {
             //playerSR.flipX = false;
+            
             Vector3 scaler = transform.localScale;
             scaler.x = 1f;
             transform.localScale = scaler;
+            
         }
         else if (Input.GetAxisRaw("Horizontal") > 0)
         {
             //playerSR.flipX = true;
+           
             Vector3 scaler = transform.localScale;
             scaler.x = -1f;
             transform.localScale = scaler;
+           
         }
 
         //handle wall jumping
         canGrab = Physics2D.OverlapCircle(wallGrabPoint.position, 2f, whatIsGround);
 
+        isGrabbing = false;
         if(canGrab && !isGrounded)
-        {/*
-            if (transform.localScale.x == 1f && Input.GetAxisRaw("Horizontal") > 0) || (transform.localScale.x == -1f && Input.GetAxisRaw("Horizontal") < 0)
+        {
+            Debug.Log("äpple");
+            if ((transform.localScale.x == 1f && Input.GetAxisRaw("Horizontal") > 0) || (transform.localScale.x == -1f && Input.GetAxisRaw("Horizontal") < 0))
             {
-                isGrabing = true;
-                https://www.youtube.com/watch?v=uNJanDrjMgU
-            }*/
+                isGrabbing = true;
+                //https://www.youtube.com/watch?v=uNJanDrjMgU
+            }
+        }
+
+        if (isGrabbing)
+        {
+            rb.gravityScale = 0f;
+            rb.velocity = Vector2.zero;
+        }
+        else
+        {
+            rb.gravityScale = gravityStore;
         }
     }
 
