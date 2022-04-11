c85130548f8f3a1a24841e9214ad5e9596095b4c
Jonas <jonas.illipe@elev.ga.lbs.se>
2022-04-11 03:54:08 +0200

    Tiles, EnemyWolf, Camera
    New tiles with grass.
    Created test map with new tiles, corners overlap with ground and wall.
    Created camera folder for parallax and more...

--- Silva Backwoods PRRPRR02/Assets/Script/Enemies/EnemyManager.cs
+++ Silva Backwoods PRRPRR02/Assets/Script/Enemies/EnemyManager.cs
@@ -9,19 +9,27 @@ public class EnemyManager : MonoBehaviour
 
     public int damage;
 
+    public float moveSpeed;
+    public float stoppDistance;
+
     protected Rigidbody2D rb;
 
     protected GameObject player;
+    protected Transform playerLocation;
     public float locateRange;
     public Transform locatePoint;
     public LayerMask playerLayer;
-    protected bool playerDetected = false;
+    protected bool playerDetected;
+
+    public Transform meleePoint;
+    public float meleeRange;
     
 
     // Start is called before the first frame update
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player");
+        playerLocation = player.GetComponent<Transform>();
         rb = GetComponent<Rigidbody2D>();
         currentHealth = maxHealth;
     }
@@ -34,22 +42,37 @@ public class EnemyManager : MonoBehaviour
             currentHealth = maxHealth;
         }
 
-        playerDetected = Physics2D.OverlapCircle(transform.position, locateRange, playerLayer);
+        playerDetected = Physics2D.OverlapCircle(locatePoint.position, locateRange, playerLayer);
+        /*if (Physics2D.OverlapCircle(locatePoint.position, locateRange, playerLayer))
+        {
+            playerDetected = Physics2D.OverlapCircle(locatePoint.position, locateRange, playerLayer);
+            Debug.Log("PlayerDetected In OverlapCircle");
+        }*/
 
         if (playerDetected == true)
         {
+            Debug.Log("PlayerDetection Is True");
             FollowPlayer();
+        }
+
+        if (Physics2D.OverlapCircle(meleePoint.position, meleeRange, playerLayer))
+        {
+            Debug.Log("Melee Detection Working");
+            MeleeAttack();
         }
     }
 
     void FollowPlayer()
     {
-
+        if (Vector2.Distance(transform.position, playerLocation.position) > stoppDistance)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, playerLocation.position, moveSpeed * Time.deltaTime);
+        }
     }
 
     void MeleeAttack()
     {
-
+        
     }
 
     public void TakeDamage(int damage)
@@ -71,5 +94,6 @@ public class EnemyManager : MonoBehaviour
     {
         Gizmos.color = Color.red;
         Gizmos.DrawWireSphere(locatePoint.position, locateRange);
+        Gizmos.DrawWireSphere(meleePoint.position, meleeRange);
     }
 }
--- Silva Backwoods PRRPRR02/Assets/Script/Enemies/Wolf/WolfEnemy.cs
+++ Silva Backwoods PRRPRR02/Assets/Script/Enemies/Wolf/WolfEnemy.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class WolfEnemy : EnemyManager
 {
-    public Transform MeleeAreaPoint;
 
     // Start is called before the first frame update
     void Start()
