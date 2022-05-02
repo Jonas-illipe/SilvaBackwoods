0aa2802548e3c9a400927da5710dcd881cb5c2e1
Jonas <jonas.illipe@elev.ga.lbs.se>
2022-05-03 01:41:28 +0200

    Lights, MainMenu
    Added texture to main menu.
    Added light to torches and made them a prefab.
    Made working sprite lights.

--- Silva Backwoods PRRPRR02/Assets/Script/Camera/Parallax.cs
+++ Silva Backwoods PRRPRR02/Assets/Script/Camera/Parallax.cs
@@ -7,6 +7,7 @@ public class Parallax : MonoBehaviour
     private float length, startpos;
     public GameObject cam;
     public float parallaxEffect;
+    public float yFollow;
 
     // Start is called before the first frame update
     void Start()
@@ -21,9 +22,16 @@ public class Parallax : MonoBehaviour
         float temp = (cam.transform.position.x * (1 - parallaxEffect));
         float dist = (cam.transform.position.x * parallaxEffect);
 
+       // float temp2 = (cam.transform.position.y * (1 - parallaxEffect));
+        //float dist2 = (cam.transform.position.y * parallaxEffect);
+        //transform.position = new Vector3(transform.position.x, startpos + dist2, transform.position.z);
+
         transform.position = new Vector3(startpos + dist, transform.position.y, transform.position.z);
 
         if (temp > startpos + length) startpos += length;
         else if (temp < startpos - length) startpos -= length;
+        /*
+        if (temp2 > startpos + length) startpos += length;
+        else if (temp2 < startpos - length) startpos -= length;*/
     }
 }
